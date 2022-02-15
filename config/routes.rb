bc88a14a62420416f79dd1dfe4f0bf14a4999c22
MisonoTakezo <misonotakezo1213@gmail.com>
2022-02-15 13:29:07 +0900

    graphql init

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,7 @@
 Rails.application.routes.draw do
+  # GraphQL
+  if Rails.env.development?
+    mount GraphiQL::Rails::Engine, at: '/graphiql', graphql_path: '/graphql'
+  end
+  post "/graphql", to: "graphql#execute"
 end
