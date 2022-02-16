83fe795a5e00631f18e5e1d4ba301eaa11d84441
MisonoTakezo <misonotakezo1213@gmail.com>
2022-02-16 15:13:07 +0900

    total posts count

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -6,11 +6,16 @@ module Types
     field :name, String
     field :email, String
     field :posts, [Types::PostType], null: true
+    field :total_posts, Integer, null: false
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
 
     def posts
       Loaders::AssociationLoader.for(User, :posts).load(object)
+    end
+
+    def total_posts
+      Loaders::AssociationCountLoader.for(User, :posts).load(object)
     end
   end
 end
